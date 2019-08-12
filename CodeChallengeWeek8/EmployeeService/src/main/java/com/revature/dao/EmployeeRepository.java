0e12c3688a5604ee8af2c509252b457e73fba8ee
Mustafa Nofel <mustafa.e.nofal@gmail.com>
2019-08-12 19:04:54 -0400

    first push

---
+++ CodeChallengeWeek8/EmployeeService/src/main/java/com/revature/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.beans.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository{
+
+	
+	public List<Employee> getAllEmployees();
+}
---
+++ CodeChallengeWeek8/ReimbursmentService/src/main/java/com/revature/ReimbursmentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ReimbursmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimbursmentServiceApplication.class, args);
+	}
+
+}
