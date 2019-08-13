99a724ab42a5c636a0bea0b16ca2859bff0110b3
Mustafa Nofel <mustafa.e.nofal@gmail.com>
2019-08-12 20:46:23 -0400

    adds sql files

---
+++ CodeChallengeWeek8/ReimbursmentService/src/main/resources/reimbursements.sql
@@ -0,0 +1,6 @@
+INSERT INTO REIMBURSEMENT (id, employeeId, amount) VALUES (1, 1, 200);
+INSERT INTO REIMBURSEMENT (id, employeeId, amount) VALUES (1, 2, 300);
+INSERT INTO REIMBURSEMENT (id, employeeId, amount) VALUES (1, 2, 400);
+INSERT INTO REIMBURSEMENT (id, employeeId, amount) VALUES (1, 3, 100);
+INSERT INTO REIMBURSEMENT (id, employeeId, amount) VALUES (1, 3, 500);
+COMMIT;
